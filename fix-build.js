541ef79fc94847677b0dce4c0beb0f925177ece9
praise-cloud <georgewillpraise1@gmail.com>
2026-03-22 16:45:57 +0100

    update

---
+++ fix-build.js
@@ -0,0 +1,10 @@
+﻿const fs = require('fs');
+const pkg = JSON.parse(fs.readFileSync('package.json', 'utf8'));
+pkg.build.win.target = [{ target: 'portable', arch: ['x64'] }];
+pkg.build.artifactName = '--portable.';
+delete pkg.build.nsis;
+fs.writeFileSync('package.json', JSON.stringify(pkg, null, 2));
+console.log('Done');
+console.log('target:', JSON.stringify(pkg.build.win.target));
+console.log('artifactName:', pkg.build.artifactName);
+console.log('nsis exists:', !!pkg.build.nsis);
